535daf1fd495f54e3fc8e909d8a557ad54383d43
mariaesa2 <maria.santos.alves07@escola.pr.gov.br>
2025-06-29 20:50:43 +0000

    aula 1 e aula 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavra.addEventListener('click',mostraPalavrasChave);
+
+function mostraPalavrasChave (){
+    alert('Fui clicado! ');
+}
